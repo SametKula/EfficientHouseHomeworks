df91d8ed88d6abc77126c8dbbacb71f8e5dfbb78
Samet Kula <sametkula@proton.me>
2022-09-27 04:40:02 +0300

    homework project 4th commit

---
+++ src/Hmwk2/Main5.java
@@ -0,0 +1,20 @@
+package Hmwk2;
+
+public class Main5 {
+    public static void main(String[] args) {
+        //paragraph
+        String str1= "I hate everything , I hate purple, I hate tarhana soup, I hate etliekmek";
+        //replace words
+        String word1 = "hate" , word2 = "love";
+        System.out.printf("Please enter two words you want to change between%ninput type is (paragraph,word one,word two)%n>>>\"%s\",%s,%s%n",str1,word1,word2);
+        //replace field
+        str1 = str1.replaceAll(word1,word2);
+        //result
+        System.out.println(str1);
+        //system out is
+        /*Please enter two words you want to change between
+        input type is (paragraph,word one,word two)
+        >>>"I hate everything , I hate purple, I hate tarhana soup, I hate etliekmek",hate,love
+        I love everything , I love purple, I love tarhana soup, I love etliekmek*/
+    }
+}
