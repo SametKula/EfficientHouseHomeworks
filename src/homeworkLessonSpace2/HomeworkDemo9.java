0027fbc4d30188e58362cbb0d372e4a12e7903c6
Samet Kula <sametkula@proton.me>
2022-09-30 22:23:42 +0300

    bike speed calculator Homework

--- src/homeworkLessonSpace2/HomeworkDemo9.java
+++ src/homeworkLessonSpace2/HomeworkDemo9.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 public class HomeworkDemo9 {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
+        System.out.println("Welcome to speed calculator");
         while (true){
             int gearSize = 0, cendence = 0;
+            System.out.print("Gear Size :");
             gearSize = keyboard.nextInt();
+            System.out.print("Cendence :");
             cendence = keyboard.nextInt();
             System.out.printf("your speed is : %.0f %n ",speedCalculator(gearSize,cendence));
         }
