4f6a7f7d3231a2d9ecb0746da7203ecddf0e868e
Samet Kula <sametkula@proton.me>
2022-09-30 22:40:23 +0300

    seconds to hours-minutes-seconds Homework

--- src/homeworkLessonSpace2/HomeworkDemo10.java
+++ src/homeworkLessonSpace2/HomeworkDemo10.java
@@ -8,7 +8,7 @@ public class HomeworkDemo10 {
         int time = keyboard.nextInt();
         int hours , minutes , seconds;
         seconds =  time % 60;
-        minutes =  time /60;// 120
+        minutes =  time /60;// 1
         hours =   minutes/60;
         minutes = minutes%60;
         System.out.printf("%d Hours, %d Minutes, %d Seconds ",hours,minutes,seconds);
