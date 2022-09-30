08251f5023d7c5e6e2decb4f89a2ab95cc844e83
Samet Kula <sametkula@proton.me>
2022-09-30 22:53:09 +0300

    pace calculator conventions, constant naming ,conventions, and formatting adjust Homework

---
+++ src/homeworkLessonSpace2/HomeworkDemo11.java
@@ -0,0 +1,12 @@
+package homeworkLessonSpace2;
+
+public class HomeworkDemo11 {
+    public static void main(String[] args) {
+        double time , pace;
+        System.out.println("This program calculates your pace given a time and distance traveled.");
+        time = 35.5; /*35 minutes and 30 seconds*/
+        pace = time /distance;
+        System.out.printf("Your pace is %.2f miles per hour.",pace);
+    }
+    public static final double distance = 6.21;
+}
