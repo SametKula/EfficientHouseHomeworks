6fc1d9c3528c4332af74e8ab899abf71d88f59d6
Samet Kula <sametkula@proton.me>
2022-10-01 20:33:10 +0300

    homeworkdemo3 remake

---
+++ src/homeworkLessonSpace2/HomeworkDemo3v2.java
@@ -0,0 +1,29 @@
+package homeworkLessonSpace2;
+
+public class HomeworkDemo3v2 {
+    public static void main(String[] args) {
+        String str1 = "walt";
+        String str2 = "disktrup";
+        String str3 = "kamil";
+        String str4 = "ulvi";
+        String str5 = "mesken";
+        String str6 = "kadir";
+        String str7 = "namık";
+        String str8 = "berke";
+        System.out.println(latinPiganator(str1,str2,str3,str4,str5,str6,str7,str8));
+
+
+
+    }
+
+    public static String latinPiganator(String... strings) {
+        String latinPigWords = "";
+        for (String string : strings) {
+            String str3 = string.substring(1, 2).toUpperCase() + string.substring(2) + string.substring(0, 1).concat("ay");
+            latinPigWords += str3.concat(" ");
+        }
+
+
+        return latinPigWords;
+    }
+}
