662f97c209c5e4e3238eb985054fe7a52898d714
Samet Kula <sametkula@proton.me>
2022-10-02 22:48:47 +0300

    Self Exercises page 61

--- src/homeworkLessonSpace2/HomeworkDemo3v2.java
+++ src/homeworkLessonSpace2/HomeworkDemo3v2.java
@@ -2,19 +2,22 @@ package homeworkLessonSpace2;
 
 public class HomeworkDemo3v2 {
     public static void main(String[] args) {
-        String str1 = "walt";
+        /*String str1 = "walt";
         String str2 = "disktrup";
         String str3 = "kamil";
         String str4 = "ulvi";
         String str5 = "mesken";
         String str6 = "kadir";
         String str7 = "namık";
-        String str8 = "berke";
-        System.out.println(latinPiganator(str1,str2,str3,str4,str5,str6,str7,str8));
-
-
+        String str8 = "berke";*/
+        //System.out.println(latinPiganator(str1,str2,str3,str4,str5,str6,str7,str8));
+        String str = "    Hello word   ";
+        String str1 = str.trim().toUpperCase().substring(0,str.trim().indexOf(' '));
+        System.out.printf("%s", str1);
+        
 
     }
+
 
     public static String latinPiganator(String... strings) {
         String latinPigWords = "";
@@ -22,6 +25,8 @@ public class HomeworkDemo3v2 {
             String str3 = string.substring(1, 2).toUpperCase() + string.substring(2) + string.substring(0, 1).concat("ay");
             latinPigWords += str3.concat(" ");
         }
+
+
 
 
         return latinPigWords;
---
+++ src/selfExercises/ChapterTwoExercises.java
@@ -0,0 +1,26 @@
+package selfExercises;
+
+public class ChapterTwoExercises {
+    public static void main(String[] args) {
+        // self exercises page 61
+//      String s = "Hello" + "Joe";
+//      System.out.println(s);
+
+//      System.out.println("One two buckle your shoe.\nThere four shut the door.");
+
+        //System.out.println( 2+ " " + 2); // if string concant a int int turn to int
+       // System.out.println(2+2);// but this seciation just 2 addition 2
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
