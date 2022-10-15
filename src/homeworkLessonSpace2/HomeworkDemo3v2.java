f608a664ee251ca3444aa4d61583a42ff7996d14
Samet Kula <sametkula@proton.me>
2022-10-15 20:32:33 +0300

    homework review pt4

--- src/homeworkLessonSpace2/HomeworkDemo3v2.java
+++ src/homeworkLessonSpace2/HomeworkDemo3v2.java
@@ -1,20 +1,21 @@
 package homeworkLessonSpace2;
 
+import java.util.Scanner;
+
 public class HomeworkDemo3v2 {
     public static void main(String[] args) {
-        /*String str1 = "walt";
-        String str2 = "disktrup";
-        String str3 = "kamil";
-        String str4 = "ulvi";
-        String str5 = "mesken";
-        String str6 = "kadir";
-        String str7 = "namık";
-        String str8 = "berke";*/
-        //System.out.println(latinPiganator(str1,str2,str3,str4,str5,str6,str7,str8));
-        String str = "    Hello word   ";
-        String str1 = str.trim().toUpperCase().substring(0,str.trim().indexOf(' '));
-        System.out.printf("%s", str1);
-        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Welcome to piglatinator \nplease enter your want to transform to piglatinator (end code is \"end\")\n");
+        while (true) {
+            System.out.print("input:");
+            String input = keyboard.nextLine();
+            if (input.equals("end")) break;
+            System.out.print("output:");
+            System.out.print(latinPiganator(input) + " \n");
+
+
+        }
+
 
     }
 
@@ -25,8 +26,6 @@ public class HomeworkDemo3v2 {
             String str3 = string.substring(1, 2).toUpperCase() + string.substring(2) + string.substring(0, 1).concat("ay");
             latinPigWords += str3.concat(" ");
         }
-
-
 
 
         return latinPigWords;
