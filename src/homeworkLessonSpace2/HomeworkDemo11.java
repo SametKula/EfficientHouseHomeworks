70142d284b70ef57384c3f809a540a927dd39014
Samet Kula <sametkula@proton.me>
2022-10-15 21:57:20 +0300

    homework review pt7

--- src/homeworkLessonSpace2/HomeworkDemo11.java
+++ src/homeworkLessonSpace2/HomeworkDemo11.java
@@ -1,12 +1,18 @@
 package homeworkLessonSpace2;
 
+import java.util.Scanner;
+
 public class HomeworkDemo11 {
     public static void main(String[] args) {
-        double time , pace;
+        Scanner keyboard = new Scanner(System.in);
+        double time , pace, distance;
+
         System.out.println("This program calculates your pace given a time and distance traveled.");
-        time = 35.5; /*35 minutes and 30 seconds*/
+        System.out.print("time:");
+        time = keyboard.nextInt(); /*35 minutes and 30 seconds*/
+        System.out.print("ditance:");
+        distance = keyboard.nextDouble();
         pace = time /distance;
         System.out.printf("Your pace is %.2f miles per hour.",pace);
     }
-    public static final double distance = 6.21;
 }
