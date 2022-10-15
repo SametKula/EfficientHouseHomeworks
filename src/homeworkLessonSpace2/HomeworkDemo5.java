2f4bdd74d74e70315bf564332c268dfae51bc6b9
Samet Kula <sametkula@proton.me>
2022-10-15 21:51:30 +0300

    homework review pt6

--- src/homeworkLessonSpace2/HomeworkDemo5.java
+++ src/homeworkLessonSpace2/HomeworkDemo5.java
@@ -1,14 +1,27 @@
 package homeworkLessonSpace2;
 
+import java.util.Scanner;
+
 public class HomeworkDemo5 {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
         //paragraph
-        String str1= "I hate everything , I hate purple, I hate tarhana soup, I hate etliekmek";
         //replace words
-        String word1 = "hate" , word2 = "love";
-        System.out.printf("Please enter two words you want to change between%ninput type is (paragraph,word one,word two)%n>>>\"%s\",%s,%s%n",str1,word1,word2);
+        System.out.printf("Please enter a paragraph and two words you want to change between%ninput type is (paragraph,word one,word two)%n");
+        System.out.print("paragraph:");
+        String str1 = keyboard.nextLine();
+
+
+        System.out.print("want to be change:");
+        String word1 = keyboard.nextLine();
+
+
+        System.out.print("I want it to be changed:");
+        String word2 = keyboard.nextLine();
+
+
         //replace field
-        str1 = str1.replaceAll(word1,word2);
+        str1 = str1.replaceAll(word1, word2);
         //result
         System.out.println(str1);
         //system out is
