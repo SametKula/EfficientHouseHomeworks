c812521254ff1ea86a4ba1972171cc05b5d3df1f
Samet Kula <sametkula@proton.me>
2022-10-15 20:55:07 +0300

    homework review pt5

--- src/homeworkLessonSpace2/HomeworkDemo4.java
+++ src/homeworkLessonSpace2/HomeworkDemo4.java
@@ -1,19 +1,28 @@
 package homeworkLessonSpace2;
 
+import java.util.Scanner;
+
 public class HomeworkDemo4 {
     public static void main(String[] args) {
-        //variables
-        int firstWeight = 100;
-        int desiredWeight = 90;
-        int elapsedTime = 10;
+        Scanner keyboard = new Scanner(System.in);
         //system message
-        System.out.printf("Welcome to soda pop risk calculator%nPlease enter your present weight ,the weight you want to be and probably time will pass%n>>>%d,%d,%d %n---------------------------%n",firstWeight,desiredWeight,elapsedTime);
+        System.out.printf("%nWelcome to soda pop risk calculator%nPlease enter your present weight ,the weight you want to be and probably time will pass%n");
+
+        //variables
+        System.out.print("current weight:");
+        int firstWeight = keyboard.nextInt();
+        System.out.print("desired weight:");
+        int desiredWeight = keyboard.nextInt();
+        System.out.print("when the diet passes:");
+        int elapsedTime = keyboard.nextInt();
 
 
         //risk calculator
+
+
         int result = (firstWeight-desiredWeight)/elapsedTime;
         //if else statement
-        String finalResult = result==0?"you may die":"not risky";
+        String finalResult = result == 0 ?"not risky":"risky";
         System.out.println(finalResult);
         /*System output is:
         Please enter your present weight ,the weight you want to be and probably time will pass
