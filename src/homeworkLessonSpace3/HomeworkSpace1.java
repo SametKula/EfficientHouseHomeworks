d5a31bf0ac64e7f4cb09bd9103aba9bc7d1e3c33
Samet Kula <sametkula@proton.me>
2022-10-23 13:38:34 +0300

    lastest version

--- src/homeworkLessonSpace3/HomeworkSpace1.java
+++ src/homeworkLessonSpace3/HomeworkSpace1.java
@@ -1,160 +1,51 @@
 package homeworkLessonSpace3;
+
 import java.util.Scanner;
 
 public class HomeworkSpace1 {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        int numberOne , numberTwo , numberTheree , highest = 0, lowest = 0 , mid = 0;
-
-
-
-
-
-
-        // 30 60 50
-        //yanlış version
-        /*if (numberOne > numberTwo)
-        {
-            if (numberOne > numberTheree)
-            {
-                highest = numberOne;
-            }
-            if (numberTwo > numberTheree)
-            {
-                lowest = numberTheree;
-                mid = numberTwo;
-            }
-            else
-            {
-                lowest = numberTwo;
-                mid = numberTheree;
-            }
-        }
-        else //30 50 60
-        {
-            if (numberTwo > numberTheree)
-            {
-                highest = numberTwo;
-            }
-            if (numberOne > numberTheree)
-            {
-                lowest = numberTheree;
-                mid = numberOne;
-            }
-
-            else
-            {
-                lowest = numberOne;
-                mid = numberTheree;
-
-            }
-        }*/
-
-        for (;;)
-        {
-            System.out.print(_resetConsoleOut+ "number one:");
+        int numberOne, numberTwo, numberTheree, highest , lowest , mid ;
+            System.out.print(_resetConsoleOut + "number one:");
             numberOne = keyboard.nextInt();
             System.out.print("number two:");
             numberTwo = keyboard.nextInt();
             System.out.print("number theree:");
             numberTheree = keyboard.nextInt();
             System.out.println("");
-            calculator(numberOne,numberTwo,numberTheree);
-        }
-
-
-
-
-
-
-
-
-
+            calculator(numberOne, numberTwo, numberTheree);
     }
 
-    public static void calculator(int numberOne , int numberTwo , int numberTheree)
-    {
-        int highest ,lowest,midle;
-        /*int highest = 0,mid = 0,lowest = 0;
-        if (numberOne > numberTwo & numberOne > numberTheree)
-        {
-            highest = numberOne;
-            if (numberTwo > numberTheree)
-            {
-                mid = numberTwo;
-                lowest = numberTheree;
-            }
-            else
-            {
-                mid = numberTheree;
-                lowest = numberTwo;
-            }
-        }
-        else if (numberTwo > numberOne & numberTwo > numberTheree)
-        {
-            highest = numberTwo;
-            if (numberOne > numberTheree)
-            {
-                mid = numberOne;
-                lowest = numberTheree;
-            }
-            else
-            {
-                mid = numberTheree;
-                lowest = numberOne;
-            }
-        }
-        else if (numberTheree > numberTwo & numberTheree > numberOne)
-        {
-            highest = numberTheree;
-            if (numberTwo > numberTheree)
-            {
-                mid = numberOne;
-                lowest = numberTwo;
-            }
-            else
-            {
-                mid = numberTwo;
-                lowest = numberOne;
+    public static void calculator(int numberOne, int numberTwo, int numberTheree) {
+        int highest, lowest, midle;
+        highest = highest(numberTheree, highest(numberOne, numberTwo));
+        lowest = lowest(numberTheree, lowest(numberOne, numberTwo));
+        midle = mid(numberOne, numberTwo, numberTheree);
+        String str1 = highest + ">" + midle + ">" + lowest;
+        str1 = str1.equals(highest + ">" + midle + ">" + lowest) ? _greenConsoleOut + "Basarili" + _resetConsoleOut : _redConsoleOut + "reddedildi" + _resetConsoleOut;
+        System.out.printf("%d %s %d %s %d    %s %n", highest, highest == midle ? "=" : ">", midle, midle == lowest ? "=" : ">", lowest, str1);
+    }
+    public static int highest(int numberOne, int numberTwo) {
+        return numberOne > numberTwo ? numberOne : numberTwo;
+    }
 
-            }
-        }*/
-        highest = highest(numberTheree , highest(numberOne,numberTwo));
-        lowest = lowest(numberTheree, lowest(numberOne,numberTwo));
-        midle = mid(numberOne,numberTwo,numberTheree);
-        String str1 = highest+">"+midle+">"+lowest;
-        str1 = str1.equals(highest+">"+midle+">"+lowest)? _greenConsoleOut +"Basarili" + _resetConsoleOut :_redConsoleOut+ "reddedildi"+_resetConsoleOut;
-        // System.out.printf("en büyük : %d %nortanca : %d%nen küçük : %d %n", highest,mid,lowest);
-        System.out.printf("%d %s %d %s %d    %s %n",highest ,highest==midle ? "=":">" , midle , midle==lowest ? "=":">" , lowest,str1);
+    public static int lowest(int numberOne, int numberTwo) {
+        return numberOne > numberTwo ? numberTwo : numberOne;
+    }
 
-    }
-    public static int highest(int numberOne , int numberTwo )
-    {
-        return numberOne>numberTwo ? numberOne : numberTwo;
-    }
-    public static int lowest(int numberOne , int numberTwo )
-    {
-        return numberOne>numberTwo ? numberTwo : numberOne;
-    }
-    public static int mid(int numberOne , int numberTwo ,int numberTheree)
-    {
-        int highest , lowest;
-        highest = highest(numberTheree , highest(numberOne,numberTwo));
-        lowest = lowest(numberTheree, lowest(numberOne,numberTwo));
-        if (highest == numberOne )
-        {
+    public static int mid(int numberOne, int numberTwo, int numberTheree) {
+        int highest, lowest;
+        highest = highest(numberTheree, highest(numberOne, numberTwo));
+        lowest = lowest(numberTheree, lowest(numberOne, numberTwo));
+        if (highest == numberOne) {
             if (lowest == numberTwo)
                 return numberTheree;
             else return numberTwo;
-        }
-        else if (highest == numberTwo)
-        {
+        } else if (highest == numberTwo) {
             if (lowest == numberOne)
                 return numberTheree;
             else return numberOne;
-        }
-        else if (highest == numberTheree )
-        {
+        } else if (highest == numberTheree) {
             if (lowest == numberTwo)
                 return numberOne;
             else return numberTwo;
@@ -162,10 +53,10 @@ public class HomeworkSpace1 {
 
         return 0;
     }
+
     public static final String _redConsoleOut = "\u001B[31m";
     public static final String _greenConsoleOut = "\u001B[32m";
     public static final String _resetConsoleOut = "\u001B[37m";
-
 
 
 }
--- src/homeworkLessonSpace3/HomeworkSpace2.java
+++ src/homeworkLessonSpace3/HomeworkSpace2.java
@@ -5,51 +5,43 @@ import java.util.Scanner;
 public class HomeworkSpace2 {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        int numberOne , numberTwo , numberTheree;
+        int numberOne, numberTwo, numberTheree;
         numberOne = keyboard.nextInt();
         numberTwo = keyboard.nextInt();
         numberTheree = keyboard.nextInt();
-
-        System.out.printf("ortanca sayi : %d",mid(numberOne,numberTwo,numberTheree));
-
-
-
-
+        System.out.printf("ortanca sayi : %d", mid(numberOne, numberTwo, numberTheree));
     }
-    public static int mid(int numberOne,int numberTwo, int numberTheree)
-    {
-        int highest = highOne(numberOne,highOne(numberTwo,numberTheree));
-        int lowest = lowOne(numberOne,lowOne(numberTwo,numberTheree));
-        return differentOne(highest,lowest,numberOne,numberTwo,numberTheree);
+
+    public static int mid(int numberOne, int numberTwo, int numberTheree) {
+        int highest = highOne(numberOne, highOne(numberTwo, numberTheree));
+        int lowest = lowOne(numberOne, lowOne(numberTwo, numberTheree));
+        return differentOne(highest, lowest, numberOne, numberTwo, numberTheree);
     }
-    public static int highOne(int numberOne,int numberTwo){
-        if (numberOne>=numberTwo)return numberOne;
+
+    public static int highOne(int numberOne, int numberTwo) {
+        if (numberOne >= numberTwo) return numberOne;
         else return numberTwo;
     }
-    public static int lowOne(int numberOne,int numberTwo){
-        if (numberOne>=numberTwo)return numberTwo;
+
+    public static int lowOne(int numberOne, int numberTwo) {
+        if (numberOne >= numberTwo) return numberTwo;
         else return numberOne;
     }
-    public static int differentOne (int highest,int lowest, int numberOne,int numberTwo, int numberTheree){
-        if (highest == numberOne || highest == numberTwo)
-        {
-            if (lowest == numberOne || lowest == numberTwo)
-            {
+
+    public static int differentOne(int highest, int lowest, int numberOne, int numberTwo, int numberTheree) {
+        if (highest == numberOne || highest == numberTwo) {
+            if (lowest == numberOne || lowest == numberTwo) {
                 return numberTheree;
             }
         }
-        if (highest == numberOne || highest == numberTheree)
-        {
-            if (lowest == numberOne || lowest == numberTheree)
-            {
+        if (highest == numberOne || highest == numberTheree) {
+            if (lowest == numberOne || lowest == numberTheree) {
                 return numberTwo;
             }
         }
 
-        if (highest == numberTheree || highest == numberTwo)
-        {
-            if (lowest == numberTheree || lowest == numberTwo)
-            {
+        if (highest == numberTheree || highest == numberTwo) {
+            if (lowest == numberTheree || lowest == numberTwo) {
                 return numberOne;
             }
         }
