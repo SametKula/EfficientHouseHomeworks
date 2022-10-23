cefc70065f1be34dfcaeb3734ae57996f6f29fd9
Samet Kula <sametkula@proton.me>
2022-10-23 13:32:02 +0300

    lesson work space 3

---
+++ src/homeworkLessonSpace3/HomeworkSpace3.java
@@ -0,0 +1,17 @@
+package homeworkLessonSpace3;
+
+import java.util.Scanner;
+
+public class HomeworkSpace3 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("enter a number");
+        System.out.print("number: ");
+        int input = keyboard.nextInt();
+        System.out.printf("your input returns : %d",signum(input));
+    }
+    public static int signum (int number){
+        if (number == 0) return 0;
+        return number < 0 ? -1 : 1 ;
+    }
+}
