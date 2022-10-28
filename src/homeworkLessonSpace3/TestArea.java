93e7767c804d3a830f4bb958c0231273b4f0fc4b
Samet Kula <sametkula@proton.me>
2022-10-29 02:15:12 +0300

    amstrong number finder homework

---
+++ src/homeworkLessonSpace3/TestArea.java
@@ -0,0 +1,15 @@
+package homeworkLessonSpace3;
+
+import java.util.Scanner;
+
+public class TestArea {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int val , tex;
+
+        while ((val = keyboard.nextInt()) != 0) {
+            tex = 1;
+
+        }
+    }
+}
