c903761754e84688ed75488c5530058ce82b2150
Samet Kula <sametkula@proton.me>
2022-10-30 19:20:33 +0300

    prime number finder homework

--- src/homeworkLessonSpace4/HomeworkSpace2.java
+++ src/homeworkLessonSpace4/HomeworkSpace2.java
@@ -5,30 +5,39 @@ public class HomeworkSpace2 {
 
         run();
     }
-    public static void run(){
+
+    public static void run() {
         java.util.Scanner keyboard = new java.util.Scanner(System.in);
 
         System.out.print("please enter your want to know to is it a prime number: ");
-        long input = keyboard.nextLong();
+        //long input = keyboard.nextLong();
 
 
-        isPrimeToDisplay(isPrime(input));
+        for (int input = 0; input<100; input++)
+            isPrimeToDisplay(isPrime(input), input);
+
+
 
     }
-    public static boolean isPrime(long x ){
+
+    public static boolean isPrime(long x) {
+        if (x <= 1)
+            return false;
+
         if (x % 2 == 0)
             return false;
 
-        for (long i = 3; i < x / 2 ; i += 2)
+        for (long i = 3; i < x / 2; i += 2)
             if (x % i == 0)
                 return false;
 
         return true;
     }
-    public static void isPrimeToDisplay(boolean x) {
-        if (x)
-            System.out.println("this number is a Prime number");
+
+    public static void isPrimeToDisplay(boolean b, long x) {
+        if (b)
+            System.out.println(x + " is a Prime number ");
         else
-            System.out.println("this number is not a Prime number");
+            System.out.println(x + " is not a Prime number");
     }
 }
