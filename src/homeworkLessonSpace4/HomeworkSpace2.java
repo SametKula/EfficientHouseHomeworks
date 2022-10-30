79512b7b238435cbf397b73a7bf6449dc27c2cc9
Samet Kula <sametkula@proton.me>
2022-10-30 15:14:54 +0300

    prime number finder homework

--- src/homeworkLessonSpace4/HomeworkSpace2.java
+++ src/homeworkLessonSpace4/HomeworkSpace2.java
@@ -16,8 +16,10 @@ public class HomeworkSpace2 {
 
     }
     public static boolean isPrime(long x ){
+        if (x % 2 == 0)
+            return false;
 
-        for (long i = x / 2; i > 1 ; i--)
+        for (long i = 3; i < x / 2 ; i += 2)
             if (x % i == 0)
                 return false;
 
