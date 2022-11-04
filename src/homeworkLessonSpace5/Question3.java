5ff7c93eec113ef1c86ff7f84fc6daa0b34e7245
Samet Kula <sametkula@proton.me>
2022-11-04 13:56:48 +0300

    Q3 upload

--- src/homeworkLessonSpace5/Question3.java
+++ src/homeworkLessonSpace5/Question3.java
@@ -4,36 +4,38 @@ public class Question3 {
     public static final double GRAVITY_VAR = 9.81;
 
     public static void main(String[] args) {
-
+        newtonInputAndCalculate();
     }
 
 
     public static void newtonInputAndCalculate() {
+        System.out.println("Welcome to calculater of weight2newton");
+        inputLoop();
+
+
+    }
+
+    private static void inputLoop() {
         java.util.Scanner kb = new java.util.Scanner(System.in);
 
-        System.out.print("enter your want to compute place");
-        String input = kb.next();
+        System.out.print("enter your want to compute place: ");
+        String input1 = kb.next();
 
-        System.out.print("enter the thing weight");
+        System.out.print("enter the thing weight: ");
         int input2 = kb.nextInt();
 
-        computeGravity(input2, input);
-
-
+        displayGravity(input2, input1);
     }
 
-    public static void computeGravity(int x, String y) {
-        if (y.equalsIgnoreCase("earth"))
-            ;
-        else if (y.equalsIgnoreCase("moon"))
-            ;
+    public static void displayGravity(int x, String y) {
+        if (y.equalsIgnoreCase("earth") | y.equalsIgnoreCase("moon"))
+            System.out.printf("%nyour newton on %s is : %.2f %n",y,calculateGaravity(x, y));
         else
             System.out.println("please enter moon or earth");
-
     }
 
-    public static double computeGaravity(int x, String y) {
-        return y.equalsIgnoreCase("moon") ? GRAVITY_VAR * x / 6 : GRAVITY_VAR * x;
+    public static double calculateGaravity(int x, String y) {
+        return (y.equalsIgnoreCase("moon") ? (GRAVITY_VAR * x) / 6 : GRAVITY_VAR * x)/10;
     }
 
 }
