27fde8c4dd182b866ee4b95812d5f6385bb24c25
Samet Kula <sametkula@proton.me>
2022-11-06 18:23:07 +0300

    last update

--- src/homeworkLessonSpace5/Question6.java
+++ src/homeworkLessonSpace5/Question6.java
@@ -2,29 +2,33 @@ package homeworkLessonSpace5;
 
 public class Question6 {
     public static void main(String[] args) {
-        finobacciNumber(0);
+        java.util.Scanner kb = new java.util.Scanner(System.in);
+
+        System.out.print("enter your days: ");
+        int input = kb.nextInt();
+
+
+        displayIncrasePopulation(input);
     }
 
 
-    public static void finobacciNumber(int loopTimes) {
-        System.out.println(finobacciLoop(loopTimes));
-
+    public static void displayIncrasePopulation(int loopTimes) {
+        System.out.printf("your poplation become will %d entitys", populationIncraseLoop(loopTimes));
     }
 
-    public static int finobacciLoop(int x) {
+    public static int populationIncraseLoop(int x) {
         int a = 1, b = 1;
 
+        x /=5;
 
         for (int i = 1; x > i ; i++) {
-
             int c = a + b;
-            System.out.println(a*10 + " + " + b*10 + " = " + c*10 +" populasyon pound cinsinden");
+
             a = b;
             b = c;
         }
 
         return b*10;
-
     }
 
 
