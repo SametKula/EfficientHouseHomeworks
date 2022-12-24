87fc0fddb261b3e5cbdd6922f88a294ce5896df9
SametKula <sametkula@proton.me>
2022-12-24 14:01:57 +0300

    yazdığım kodu eren hocam görse taş eder xd 3

---
+++ src/homeworkLessonSpace6/Question5.java
@@ -0,0 +1,25 @@
+package homeworkLessonSpace6;
+
+public class Question5 {
+    public static void main(String[] args) {
+        run();
+    }
+    public static void run(){
+
+    }
+    public static String toplamAsallari(int x){
+        if (x%2 != 0)
+            return "çift sayi gir";
+
+
+        return " ";
+    }
+
+    public static boolean isPrime(){
+
+
+    }
+
+
+
+}
