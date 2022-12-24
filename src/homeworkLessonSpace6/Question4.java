1e05a1f6eb879803d5ee787b0233d2f4dbf00a72
SametKula <sametkula@proton.me>
2022-12-24 13:46:27 +0300

    yazdığım kodu eren hocam görse taş eder xd

---
+++ src/homeworkLessonSpace6/Question4.java
@@ -0,0 +1,37 @@
+package homeworkLessonSpace6;
+
+public class Question4 {
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run(){
+        starLeader(100,7);
+    }
+
+    public static void starLeader(int height ,int width){
+        boolean b = true;
+        for (int count = 0 , heightControl = 0; heightControl <= height ; heightControl++ ){
+            int i = 0;
+            System.out.print("/");
+            for (;i < count;i++)
+                System.out.print(" ");
+            System.out.print("*");
+            for (i += 2;i <= width;i++)
+                System.out.print(" ");
+            System.out.print("/\n");
+            if (count == width - 1)
+                b = false;
+            if (count == 1)
+                b = true;
+
+            if (b)
+                count++;
+            else
+                count--;
+
+        }
+
+
+    }
+}
