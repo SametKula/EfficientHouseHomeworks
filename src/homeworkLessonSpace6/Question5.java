ee940ffdd7fd706ce1710ef2bd0afd5306fdee0f
SametKula <sametkula@proton.me>
2022-12-24 14:10:44 +0300

    thinking

--- src/homeworkLessonSpace6/Question5.java
+++ src/homeworkLessonSpace6/Question5.java
@@ -4,22 +4,35 @@ public class Question5 {
     public static void main(String[] args) {
         run();
     }
-    public static void run(){
+
+    public static void run() {
 
     }
-    public static String toplamAsallari(int x){
-        if (x%2 != 0)
+
+    public static String toplamAsallari(int x) {
+        if (x % 2 != 0)
             return "çift sayi gir";
+        if (x < 4)
+            return "daha büyük sayı girin";
+
+        for (int a = 3, b = x - 1; a < b; a += 2, b-=2){
 
 
-        return " ";
+        }
+
+            return " ";
     }
 
-    public static boolean isPrime(){
+    public static boolean isPrime(int x) {
+        if (x % 2 == 0)
+            return x == 2;
 
+        for (int i = 3; i * i <= x; i += 2)
+            if (x % i == 0)
+                return false;
 
+        return x > 1;
     }
-
 
 
 }
