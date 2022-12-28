10a9dd90b60cf63236b4aeedb20cc2e940110b5f
SametKula <sametkula@proton.me>
2022-12-28 06:31:30 +0300

    trying new things

--- src/selfExercises/OopExercises.java
+++ src/selfExercises/OopExercises.java
@@ -15,8 +15,24 @@ class Library {
         book.name = name;
     }
 
-    public static void ratingUpdater(Book book){
+    public static void ratingAdjuster(Book book,int incraserNumber){
+        book.rating += incraserNumber;
+    }
 
+    public static void nameAdjuster(Book book, int paperCount){
+        book.paperCount = paperCount;
+    }
+
+    public static void changeWriterStatus(Book book){
+        System.out.println("writer status changed to ->" + !book.isWriterAlive);
+        book.isWriterAlive = !book.isWriterAlive;
+    }
+
+    public static int readedCount(Book book){
+        return book.whatTimesRead;
+    }
+    public static void adjustReadedCount(Book book ,int readedCount){
+        book.whatTimesRead = readedCount;
     }
 }
 
