2c2fa612a5d13231452a4c9c368a8eaf83b3c792
SametKula <sametkula@proton.me>
2022-12-28 07:12:47 +0300

    trying new things

--- src/selfExercises/OopExercises.java
+++ src/selfExercises/OopExercises.java
@@ -11,28 +11,45 @@ class App {
 }
 
 class Library {
-    public static void nameAdjuster(Book book, String name){
+    public static void nameAdjuster(Book book, String name) {
         book.name = name;
     }
 
-    public static void ratingAdjuster(Book book,int incraserNumber){
+    public static void ratingAdjuster(Book book, int incraserNumber) {
         book.rating += incraserNumber;
     }
 
-    public static void nameAdjuster(Book book, int paperCount){
+    public static void nameAdjuster(Book book, int paperCount) {
         book.paperCount = paperCount;
     }
 
-    public static void changeWriterStatus(Book book){
-        System.out.println("writer status changed to ->" + !book.isWriterAlive);
+    public static void changeWriterStatus(Book book) {
+        System.out.println("writer status changed to ->" + (book.isWriterAlive ? "Dead" : "Alive"));
         book.isWriterAlive = !book.isWriterAlive;
     }
 
-    public static int readedCount(Book book){
+    public static int readedCount(Book book) {
         return book.whatTimesRead;
     }
-    public static void adjustReadedCount(Book book ,int readedCount){
+
+    public static void adjustReadedCount(Book book, int readedCount) {
         book.whatTimesRead = readedCount;
+    }
+
+    public static void rentBook(Book book, int day, int month, int year) {
+        Date newDate;
+        newDate = new Date();
+
+        newDate.day = day;
+        newDate.month = month;
+        newDate.year = year;
+
+        if (DateValidator.isDateValid(newDate)) {
+            System.out.println("your rent proces is done have a nice day");
+            book.date = newDate;
+        } else
+            System.out.println("please enter valid date");
+
     }
 }
 
@@ -43,7 +60,7 @@ class Book {
     public int rating;
     public int whatTimesRead;
     public boolean isAvailable;
-    public String date;
+    public Date date;
 }
 
 class Date {
@@ -52,12 +69,13 @@ class Date {
     public int year;
 
 }
-class DateValidator{
-    public static boolean isDateValid(Date date){
+
+class DateValidator {
+    public static boolean isDateValid(Date date) {
         return date.day >= 1 && date.day <= 31 && 1 <= date.month && date.month <= 12 && date.day <= giveRealDay(date);
     }
 
-    public static int giveRealDay(Date date){
+    public static int giveRealDay(Date date) {
         int day = 31;
 
         switch (date.month) {
@@ -70,7 +88,8 @@ class DateValidator{
         }
         return day;
     }
-    public static boolean isLeapYear(Date date){
+
+    public static boolean isLeapYear(Date date) {
         return date.year % 4 == 0 && date.year % 100 != 0 || date.year % 400 == 0;
     }
 }
