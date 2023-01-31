818f3f6ef38054e760d59676a34389c574260799
SametKula <sametkula@proton.me>
2023-01-31 23:00:24 +0300

    multiple coin flip method added to PossibilitiesUtil class

--- src/homeworkLessonSpace8/PossibilitiesUtil.java
+++ src/homeworkLessonSpace8/PossibilitiesUtil.java
@@ -3,7 +3,7 @@ package homeworkLessonSpace8;
 
 public class PossibilitiesUtil {
 
-    public static double diceChance(int a, int loopTimes, java.util.Random rd) {
+    public static double diceChance( java.util.Random rd , int loopTimes, int a) {
         int count = 0;
 
         for (int i = 0; i < loopTimes; i++) {
@@ -17,7 +17,7 @@ public class PossibilitiesUtil {
         return count / (double) loopTimes;
     }
 
-    public static double diceChance(int a, int b, int loopTimes, java.util.Random rd) {
+    public static double diceChance(java.util.Random rd, int loopTimes, int a, int b) {
         int count = 0;
 
         for (int i = 0; i < loopTimes; i++) {
@@ -31,7 +31,7 @@ public class PossibilitiesUtil {
         return count / (double) loopTimes;
     }
 
-    public static double diceChance(int a, int b, int c, int loopTimes, java.util.Random rd) {
+    public static double diceChance(java.util.Random rd, int loopTimes, int a, int b, int c) {
         int count = 0;
 
         for (int i = 0; i < loopTimes; i++) {
@@ -45,7 +45,7 @@ public class PossibilitiesUtil {
         return count / (double) loopTimes;
     }
 
-    public static double diceChance(int a, int b, int c, int d, int loopTimes, java.util.Random rd) {
+    public static double diceChance(java.util.Random rd, int loopTimes, int a, int b, int c, int d) {
         int count = 0;
 
         for (int i = 0; i < loopTimes; i++) {
@@ -59,7 +59,7 @@ public class PossibilitiesUtil {
         return count / (double) loopTimes;
     }
 
-    public static double diceChance(int a, int b, int c, int d, int f, int loopTimes, java.util.Random rd) {
+    public static double diceChance(java.util.Random rd, int loopTimes, int a, int b, int c, int d, int f) {
         int count = 0;
 
         for (int i = 0; i < loopTimes; i++) {
@@ -73,7 +73,8 @@ public class PossibilitiesUtil {
         return count / (double) loopTimes;
     }
 
-    public static double diceChance(int a, int b, int c, int d, int f, int g, int loopTimes, java.util.Random rd) {
+    public static double diceChance(java.util.Random rd, int loopTimes, int a, int b, int c, int d, int f, int g) {
+
         int count = 0;
 
         for (int i = 0; i < loopTimes; i++) {
@@ -87,10 +88,6 @@ public class PossibilitiesUtil {
         return count / (double) loopTimes;
     }
 
-    public static int throwDice(java.util.Random rd) {
-        return rd.nextInt(6) + 1;
-    }
-
     public static double twoDiceChance(int looptimes, java.util.Random rd) {
         int count = 0;
 
@@ -99,34 +96,7 @@ public class PossibilitiesUtil {
                 count++;
 
         return count / (double) looptimes;
-    }
-
-
-    //yasaklanan java kullanimi
-
-    /**
-     * this method give odds ratio of given numbers possiblates to all sequence
-     */
-    public static double diceOddsRatio(int loopTimes, java.util.Random rd, int... ints) {
-        int count = 0;
-
-        for (int i = 0; i < loopTimes; i++)
-            for (int k = 0, throwedDice = throwDice(rd); k < ints.length; k++)
-                if (ints[k] == throwedDice)
-                    count++;
-
-        return count / (double) loopTimes;
-/*
-
-            for (int j : ints)
-                if (j == temp)
-                    count++;
-*/
-    }
-
-    public static String flipCoin(java.util.Random rd) {
-        return rd.nextBoolean() ? "heads" : "tails";
-    }
+    } 
 
     public static double coinFlipRatio(int loopTimes, String coinFace, java.util.Random rd) {
         if (!coinFace.equalsIgnoreCase("tails") || !coinFace.equalsIgnoreCase("heads"))
@@ -139,4 +109,55 @@ public class PossibilitiesUtil {
 
         return count / (double) loopTimes;
     }
+
+    public static int throwDice(java.util.Random rd) {
+        return rd.nextInt(6) + 1;
+    }
+    public static String flipCoin(java.util.Random rd) {
+        return rd.nextBoolean() ? "heads" : "tails";
+    }
+
+    public static String coinValidator (String coinFace){
+        if (coinFace.equalsIgnoreCase("heads"))
+            return "heads";
+        else if (coinFace.equalsIgnoreCase("tails"))
+            return "tails";
+        return "invalid";
+    }
+    public static double multipleCoinFlip (java.util.Random rd, int coinFlipTimes , int coinInSameTime,String wantedCoinFace){
+        int wantedCoin = 0;
+        boolean trueCoinFace = wantedCoinFace.equals("heads");
+
+        if (coinValidator(wantedCoinFace).equals("invalid"))
+            return 0;
+        else
+            wantedCoinFace = coinValidator(wantedCoinFace);
+
+        for (int i = 0; i < coinFlipTimes; i++)
+            for (int j = 0; j < coinInSameTime; j++)
+                if (flipCoin(rd).equals(wantedCoinFace))
+                    wantedCoin++;
+
+
+        return wantedCoin / (double)coinFlipTimes*coinInSameTime;
+    }
+
+
+
+    /**
+     * yasaklanan java kullanimi
+     * this method give odds ratio of given numbers possiblates to all sequence
+     */
+    public static double diceOddsRatio(int loopTimes, java.util.Random rd, int... ints) {
+        int count = 0;
+
+        for (int i = 0; i < loopTimes; i++)
+            for (int k = 0, throwedDice = throwDice(rd); k < ints.length; k++)
+                if (ints[k] == throwedDice)
+                    count++;
+
+        return count / (double) loopTimes;
+    }
+
+
 }
