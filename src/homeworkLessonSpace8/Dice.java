277cd3e56a185fc01b281a421c093b2aab0d9204
SametKula <sametkula@proton.me>
2023-02-03 22:15:21 +0300

    new thing added

--- src/homeworkLessonSpace8/Dice.java
+++ src/homeworkLessonSpace8/Dice.java
@@ -1,18 +1,14 @@
 package homeworkLessonSpace8;
 
-public class Dice {
-    private static java.util.Random r;
 
-    public Dice(){
-        r = new java.util.Random();
-        r.setSeed(42);
-    }
-    private int dice;
+public class Dice {
+    public int dice;
 
     public void roll(){
-        this.dice = r.nextInt(6) + 1;
+        java.util.Random r = new java.util.Random();
+        dice = r.nextInt(6) + 1;
     }
     public int getDice(){
-        return this.dice;
+        return dice;
     }
 }
