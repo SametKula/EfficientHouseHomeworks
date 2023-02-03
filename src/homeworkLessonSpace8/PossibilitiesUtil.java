32c9154e05276f59d85b48cb3bb37e6429381de8
SametKula <sametkula@proton.me>
2023-02-04 00:15:23 +0300

    dice class additions

--- src/homeworkLessonSpace8/PossibilitiesUtil.java
+++ src/homeworkLessonSpace8/PossibilitiesUtil.java
@@ -3,7 +3,7 @@ package homeworkLessonSpace8;
 
 public class PossibilitiesUtil {
 
-    public static double diceChance( java.util.Random rd , int loopTimes, int a) {
+    public static double diceChance(java.util.Random rd, int loopTimes, int a) {
         int count = 0;
 
         for (int i = 0; i < loopTimes; i++) {
