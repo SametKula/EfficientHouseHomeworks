107d847d330c015400c723b4302bc0aa0d033e50
SametKula <sametkula@proton.me>
2023-02-22 20:09:55 +0300

    new display methods added

--- src/homeworkLessonSpace9/OverLoadUtil.java
+++ src/homeworkLessonSpace9/OverLoadUtil.java
@@ -1,5 +1,24 @@
 package homeworkLessonSpace9;
 
 public class OverLoadUtil {
+    public static void disaplayTaxOwner(SoccerTeam... teams) {
+        System.out.printf("|%20s|%20s|%20s|%20s|%20s|\n", "Team Name", "TaxNumber", "Favorite Player", "Gain for per year", "Team's Players");
+        System.out.print("+--------------------+--------------------+--------------------+--------------------+--------------------+\n");
 
+        for (SoccerTeam team : teams) {
+            System.out.printf("|%20s|%20s|%20s|%20d|%20d|\n", team.name.toUpperCase(), team.taxNumber, team.bestPlayerName, team.gainByYear, team.playerCountTheyHave);
+            System.out.print("+--------------------+--------------------+--------------------+--------------------+--------------------+\n");
+        }
+
+
+    }
+
+    public static void disaplayTaxOwner(SoccerTeam team) {
+        System.out.printf("|%20s|%20s|%20s|%20s|%20s|\n", "Team Name", "TaxNumber", "Favorite Player", "Gain for per year", "Team's Players");
+        System.out.print("+--------------------+--------------------+--------------------+--------------------+--------------------+\n");
+        System.out.printf("|%20s|%20s|%20s|%20d|%20d|\n", team.name.toUpperCase(), team.taxNumber, team.bestPlayerName, team.gainByYear, team.playerCountTheyHave);
+        System.out.print("+--------------------+--------------------+--------------------+--------------------+--------------------+\n");
+
+
+    }
 }
--- src/homeworkLessonSpace9/Testing.java
+++ src/homeworkLessonSpace9/Testing.java
@@ -2,6 +2,11 @@ package homeworkLessonSpace9;
 
 public class Testing {
     public static void testRun(){
+        System.out.println("Welcome to testing area");
 
+
+        SoccerTeam kasimpasa = TaxFactory.create(new SoccerTeam(), "kasimpasa","icardi",14,10000);
+
+        OverLoadUtil.disaplayTaxOwner(kasimpasa);
     }
 }
