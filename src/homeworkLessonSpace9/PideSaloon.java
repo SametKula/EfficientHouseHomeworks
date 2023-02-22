bb1a4a2008980ebccc832c1917a056d6f6511dfc
SametKula <sametkula@proton.me>
2023-02-22 17:09:08 +0300

    TaxFactory finished

--- src/homeworkLessonSpace9/PideSaloon.java
+++ src/homeworkLessonSpace9/PideSaloon.java
@@ -3,7 +3,7 @@ package homeworkLessonSpace9;
 public class PideSaloon {
     public String name;
     public String taxNumber;
-    public double cookedPideByHour;
+    public double cookingPideRatio;
     public int workerCount;
-    public int pupularty;
+    public int popularity;
 }
--- src/homeworkLessonSpace9/TaxFactory.java
+++ src/homeworkLessonSpace9/TaxFactory.java
@@ -3,7 +3,7 @@ package homeworkLessonSpace9;
 import java.util.Random;
 
 public class TaxFactory {
-    public static SoccerTeam create(SoccerTeam team, String name, String bestPlayerName, int playerCount, int gainByPerYear){
+    public static SoccerTeam create(SoccerTeam team, String name, String bestPlayerName, int playerCount, int gainByPerYear) {
         team.name = name;
         team.bestPlayerName = bestPlayerName;
         team.playerCountTheyHave = playerCount;
@@ -12,11 +12,12 @@ public class TaxFactory {
 
         return team;
     }
-    public static PetStore create(PetStore petStore, String name, int animalsCount){
-        return create(petStore,name,animalsCount,false);
+
+    public static PetStore create(PetStore petStore, String name, int animalsCount) {
+        return create(petStore, name, animalsCount, false);
     }
 
-    public static PetStore create(PetStore petStore, String name, int animalsCount, boolean haveStrorage){
+    public static PetStore create(PetStore petStore, String name, int animalsCount, boolean haveStrorage) {
         petStore.name = name;
         petStore.animalsTheyHave = animalsCount;
         petStore.areTheyHaveStorage = haveStrorage;
@@ -25,22 +26,23 @@ public class TaxFactory {
         return petStore;
     }
 
+    public static PideSaloon create(PideSaloon pideci, String name, double pideRatioByhour, int workers, int popularity) {
+        pideci.name = name;
+        pideci.cookingPideRatio = pideRatioByhour;
+        pideci.workerCount = workers;
+        pideci.popularity = popularity;
+        pideci.taxNumber = taxNameCreate();
+
+        return pideci;
+    }
 
 
-
-
-
-
-
-
-
-
-    public static String taxNameCreate(){
+    public static String taxNameCreate() {
         Random rd = new Random();
-        String str = "" + (char)(rd.nextInt(9) + 49);
+        String str = "" + (char) (rd.nextInt(9) + 49);
 
         for (int i = 0; i < 8; i++)
-            str = str + (char)(rd.nextInt(9) + 48);
+            str = str + (char) (rd.nextInt(9) + 48);
 
         //its need a control condition because maybe this taxNumber alerayd have taken
         return str;
