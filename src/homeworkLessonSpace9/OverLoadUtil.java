1d3a632111cf929d824c81ccfe853174c00b7e5b
SametKula <sametkula@proton.me>
2023-02-26 20:08:51 +0300

    display method phase one is done

--- src/homeworkLessonSpace9/OverLoadUtil.java
+++ src/homeworkLessonSpace9/OverLoadUtil.java
@@ -36,15 +36,5 @@ public class OverLoadUtil {
         System.out.print("+--------------------+--------------------+--------------------+--------------------+\n");
     }
 
-    public static void disaplayTaxOwner(SoccerTeam... teams) {
-        System.out.printf("|%20s|%20s|%20s|%20s|%20s|\n", "Team Name", "TaxNumber", "Favorite Player", "Gain for per year", "Team's Players");
-        System.out.print("+--------------------+--------------------+--------------------+--------------------+--------------------+\n");
 
-        for (SoccerTeam team : teams) {
-            System.out.printf("|%20s|%20s|%20s|%20d|%20d|\n", team.name.toUpperCase(), team.taxNumber, team.bestPlayerName, team.gainByYear, team.playerCountTheyHave);
-            System.out.print("+--------------------+--------------------+--------------------+--------------------+--------------------+\n");
-        }
-
-
-    }
 }
