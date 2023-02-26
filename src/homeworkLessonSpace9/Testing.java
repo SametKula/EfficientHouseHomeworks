a5aca01424a4c42fe94c241def0890fbea734c32
SametKula <sametkula@proton.me>
2023-02-26 23:15:35 +0300

    display method phase one is done

--- src/homeworkLessonSpace9/Testing.java
+++ src/homeworkLessonSpace9/Testing.java
@@ -2,11 +2,13 @@ package homeworkLessonSpace9;
 
 public class Testing {
     public static void testRun() {
-        System.out.println("Welcome to testing area");
+//        System.out.println("Welcome to testing area");
+//        PideSaloon kasimpasa = TaxFactory.create(new PideSaloon(), "Kardesler Pide", 13.4, 23, 122);
+//
+//        OverLoadUtil.disaplayTaxOwner(kasimpasa);
+//
 
-
-        PideSaloon kasimpasa = TaxFactory.create(new PideSaloon(), "Kardesler Pide", 13.4, 23, 122);
-
-        OverLoadUtil.disaplayTaxOwner(kasimpasa);
     }
 }
+
+
