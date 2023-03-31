db32ffe16d75549edb66382eced61680436df707
SametKula <sametkula@proton.me>
2023-03-31 22:15:12 +0300

    vggg

--- src/homerworkLessonSpace10/advanceQuestion/Question1.java
+++ src/homerworkLessonSpace10/advanceQuestion/Question1.java
@@ -2,7 +2,7 @@ package homerworkLessonSpace10.advanceQuestion;
 
 public class Question1 {
     public static void main(String[] args) {
-
+        System.out.println(displayPlaces("122122122333122333", "122"));
     }
 
     public static int wordsFirstPlace(String aBigString, String finderOne) {
@@ -10,17 +10,31 @@ public class Question1 {
     }
 
     public static String displayPlaces(String aBigString, String wanted) {
-        int count = 0;
-        String places = "";
+        int count = 0, countTemp = 0;
+        String places = "", temp = aBigString;
 
-        for (int i = 0; i < aBigString.length(); i++)
-            if (wordsFirstPlace(aBigString.substring(i), wanted) == 0) {
-                places += String.format("the number the number is %d%n",i);
-                count++;
-            }
-        places += "and there are " + count + " wanted numbers";
+//        for (int i = 0; i < aBigString.length(); i++)
+//            if (wordsFirstPlace(aBigString.substring(i), wanted) == 0) {
+//                places += String.format("the number the number is %d%n",i);
+//                count++;
+//            }
+//        places += "and there are " + count + " wanted numbers";
 
-        return places;
+        while(true){
+            int wantedOrder = wordsFirstPlace(temp, wanted);
+
+            countTemp += countTemp == 0 ? wantedOrder : wantedOrder + 1;
+            if (wantedOrder == -1)
+                break;
+
+            places += "in this order there is a word " + countTemp + "\n";
+
+            temp = temp.substring(wantedOrder + 1);
+
+            count++;
+        }
+
+        return places + "  \nand there is wanted"+count;
     }
 
     public static String reversed(String wanted){
---
+++ src/homerworkLessonSpace10/interviewProject/Tst.java
@@ -0,0 +1,39 @@
+package homerworkLessonSpace10.interviewProject;
+
+public class Tst {
+    public static void main(String[] args)
+    {
+        System.out.println(helloTo("Eren", 31, 3));
+        System.out.println(getLongestPalindrom("ahmet eren adaedipadakisikalipapilakisikada"));
+    }
+    public static String helloTo(String name, int day, int month){
+        return String.format("hello %s (%d / %d)",name ,day, month);
+    }
+    public static String reverse(String text){
+        String result = "";
+
+        for (int i = text.length() - 1; i >= 0; i--){
+            result += text.charAt(i);
+        }
+
+        return result;
+    }
+    public static boolean isPalindrom(String str){
+        return str.equals(reverse(str)) && str.length() > 1;
+    }
+    public static String getLongestPalindrom(String text){
+        String result = "";
+
+        for (int i = 0; i < text.length(); i++)
+            for (int j = text.length(); j > i ; j--){
+                String temp = text.substring(i, j);
+                if (isPalindrom(temp) && result.length() < temp.length())
+                    result = temp;
+            }
+
+        return result;
+    }
+
+
+
+}
