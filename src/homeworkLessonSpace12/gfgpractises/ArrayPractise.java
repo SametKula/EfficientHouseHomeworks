19f5a4db92cac2bad4d8eaa4b2ce4494a7ec3d6d
SametKula <sametkula@proton.me>
2023-05-19 20:04:40 +0300

    missing element now working

--- src/homeworkLessonSpace12/gfgpractises/ArrayPractise.java
+++ src/homeworkLessonSpace12/gfgpractises/ArrayPractise.java
@@ -1,7 +1,6 @@
 package homeworkLessonSpace12.gfgpractises;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -169,31 +168,20 @@ public class ArrayPractise {
     }
 
     public static int getMissingElement(int[] array) {
-        Arrays.sort(array);
+        NUMBER:
+        for (int i = 1; i <= array.length; i++) {
+            for (int j = 0; j < array.length; j++)
+                if (i == array[j])
+                    continue NUMBER;
 
-        ELEMENT:
-        for (int i = 0; i < array.length; i++) {
-            boolean flag = false;
-            int temp = -2;
-            for (int j = 1; j <= array.length; j++) {
-                flag = array[i] == j;
-                if (flag) {
-                    temp = j;
-                    continue ELEMENT;
-                }
-            }
-            if (flag){
-                return temp;
-            }
+            return i;
         }
         return -1;
     }
 }
-
-
 class ArrayTest {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100};
 
         System.out.println(ArrayPractise.getMissingElement(arr));
         //String[] arr = {"Salda" , "Isa" , "Asil", "Kübra", "Elif"};
