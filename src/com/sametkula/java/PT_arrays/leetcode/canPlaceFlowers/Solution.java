47653db7105512531c4bc75f7f2c87a1d713a487
SametKula <sametkula@proton.me>
2023-06-14 17:08:51 +0300

    codes(learning):  leetcode Can Place Flowers  solved

---
+++ src/com/sametkula/java/PT_arrays/leetcode/canPlaceFlowers/Solution.java
@@ -0,0 +1,17 @@
+package com.sametkula.java.PT_arrays.leetcode.canPlaceFlowers;
+
+public class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(n == 0) return true;
+
+        for(int i = 0; i < flowerbed.length; i++) {
+            if(i == 0 || flowerbed[i -1] == 0 && flowerbed[i] == 0 && i < flowerbed.length - 1 ||flowerbed[i + 1] == 0){
+                flowerbed[i] = 1;
+                n--;
+                if (n == 0) return true;
+            }
+
+        }
+        return false;
+    }
+}
