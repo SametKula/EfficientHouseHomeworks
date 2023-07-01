fa4ce4f665d161694b02af632819b926186a5ff5
SametKula <sametkula@proton.me>
2023-07-01 20:29:54 +0300

    codes(learning):  im trying to solve Max Number of K-Sum Pairs

---
+++ src/com/sametkula/java/PT_arrays/leetcode/maxNumberOfKSumPairs/Solution.java
@@ -0,0 +1,18 @@
+package com.sametkula.java.PT_arrays.leetcode.maxNumberOfKSumPairs;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int maxOperations(int[] nums, int k){
+        int i=0;
+        int j = nums.length-1;
+        int count=0;
+
+        Arrays.sort(nums);
+
+        while(i<j){
+
+        }
+        return count;
+    }
+}
